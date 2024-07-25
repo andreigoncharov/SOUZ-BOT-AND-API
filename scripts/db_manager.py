48fd4762b12b1a7de9bf1676689555a4b837770e
andreigoncharov <anderigoncharov1009@gmail.com>
2024-07-25 15:57:28 -0700

    формирование отчета по предыдущему дню

--- scripts/db_manager.py
+++ scripts/db_manager.py
@@ -219,8 +219,8 @@ WHERE [id] = '{expeditor_id}';""")
     async def get_point_by_dock(self, doc, loop):
         conn = self.get_conn(loop)
         cursor = conn.cursor()
-        cursor.execute(f"""SELECT [Route], [Order] FROM [Orders].[dbo].[yesterday_bot_view] WHERE 
-        [DockNo] = '{doc}';""")
+        cursor.execute(f"""SELECT CAST(SP1797 AS INT), CAST(SP1197 AS INT) FROM [192.168.3.18].[SOUZ].dbo.[sc137]
+         WHERE [IDDOC] = '{doc}';""")
         rows = cursor.fetchall()
         conn.close()
         return rows
--- test.py
+++ test.py
@@ -1,20 +1,36 @@
-import re
-
-text = '067 952 71 00'
-
-
-def normalize_phone_number_plus(phone_number):
-    cleaned_number = re.sub(r'\D', '', phone_number)
-
-    if len(cleaned_number) == 10 and cleaned_number.startswith('0'):
-        normalized_number = '+380' + cleaned_number[1:]
-    elif len(cleaned_number) == 12 and cleaned_number.startswith('380'):
-        normalized_number = '+380' + cleaned_number[3:]
-    elif len(cleaned_number) == 13 and cleaned_number.startswith('+380'):
-        normalized_number = cleaned_number
-    else:
-        return ''
-    return normalized_number
-
-
-print(normalize_phone_number_plus(text))
+# SELECT
+#      CAST(SUBSTRING(j.DATE_TIME_IDDOC, 1, 8) AS DATETIME) as [DateTime]
+#     ,e.id ExpeditorId
+#     ,e.DESCR as Expeditor
+#     ,p.sp294 as PhoneNumber
+#     ,RTRIM(LTRIM(c.code)) CustomerId
+#     ,c.DESCR as Customer
+# -- ,i.sp1035 as Comment
+#     ,CAST(SP1197 AS INT) [Order]
+#     ,CAST(SP1797 AS INT) [Route]
+#     ,drv.ID as DriverID
+#     ,drv.DESCR as Driver
+#     ,i.SP1408 as AgentID
+#     ,managers.DESCR
+#     ,chk.[TimeStamp]
+#     ,chk.[DockNo]
+# FROM [192.168.3.18].[SOUZ].dbo.[sc703] e
+# INNER JOIN [192.168.3.18].[SOUZ].dbo.[dh640] i
+#   ON i.SP702 = e.id
+# inner join [192.168.3.18].[SOUZ].dbo.[sc284] p
+# on e.sp705 = p.id
+# INNER JOIN [192.168.3.18].[SOUZ].dbo.[_1SJOURN] j
+#   ON j.iddoc = i.iddoc
+#     AND j.CLOSED = 1
+#     AND CAST(SUBSTRING(j.DATE_TIME_IDDOC, 1, 8) AS DATE) = CAST('20240404' AS date)--cast(getDATE() as date)
+# INNER JOIN [192.168.3.18].[SOUZ].dbo.[sc137] c
+#   ON c.ID = i.SP643
+# INNER JOIN [192.168.3.18].[SOUZ].dbo.SC580 as drv
+#   ON drv.ID = i.SP650
+# INNER JOIN [192.168.3.18].[Souz].dbo.SC885 as managers
+#   ON managers.ID = i.sp1408
+# left JOIN ExpeditorCheckouts chk
+#   ON chk.CustomerId = RTRIM(LTRIM(c.code)) AND
+#      chk.ExpeditorId = e.id and
+#      cast(chk.TimeStamp as DATE) = CAST('20240404' AS date)
+# order by ExpeditorId, CAST(SP1797 AS INT), CAST(SP1197 AS INT)
